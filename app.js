fee7715670fe3c2e76b82bcba109f5e7321dfb73
alinauman219 <alinauman219@gmail.com>
2021-04-21 00:17:59 +0000

    comments

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var cors = require('cors');
 require('dotenv').config();
 const app = express();
 
+//Different middlewares to ensure 
+//proper functioning of the app
 app.use(cors());
 app.use(express.json());
 app.use(function (req, res, next) {
@@ -11,16 +13,22 @@ app.use(function (req, res, next) {
     next();
 });
 
+//MongoDB Atlas Connection URI
 const uri = process.env.URI;
 
+//Conncetion to the DB
+//at above URI using mongoose
 mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log(`Database Connected.`));
 
+//Setting up server on a PORT to 
+//listen and respond to incomming requests
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () =>
   console.log(`Server up on port ${PORT}...`)
 );
 
+//api endpoint with url => "/"
 app.get("/", async (req, res) => {
     res.status(200).send("Working Fine");
 });
